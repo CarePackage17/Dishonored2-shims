4f23a80a568b94d50ed560a1cc847a35b7ca9987
CarePackage17 <wave.dev@hotmail.com>
2016-09-24 17:18:34 +0200

    some debug output for iggy functions (crashes at startup)

--- iggyperfmon_w64/dllmain.cpp
+++ iggyperfmon_w64/dllmain.cpp
@@ -1,36 +1,45 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
+#include <stdio.h>
+#include <cstdint>
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
 {
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
-    }
-    return TRUE;
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
 }
 
-extern "C" 
+extern "C"
 {
-    _declspec(dllexport) void IggyPerfmonCreate()
-    {
-        OutputDebugStringA(__FUNCTION__);
-    }
+	_declspec(dllexport) UINT64 IggyPerfmonCreate(void* pfnMalloc, void* pfnFree, void* ugh)
+	{
+		char buf[512]{};
+		sprintf_s(buf, "%s: malloc address: %p, free address: %p, last param: %p", __FUNCTION__, pfnMalloc, pfnFree, ugh);
 
-    _declspec(dllexport) void IggyPerfmonDestroy()
-    {
-        OutputDebugStringA(__FUNCTION__);
-    }
+		OutputDebugStringA(buf);
+		return 1;
+	}
 
-    _declspec(dllexport) void IggyPerfmonTickAndDraw()
-    {
-        OutputDebugStringA(__FUNCTION__);
-    }
+	_declspec(dllexport) void IggyPerfmonDestroy()
+	{
+		OutputDebugStringA(__FUNCTION__);
+	}
+
+	_declspec(dllexport) void IggyPerfmonTickAndDraw(void* ptr0, void* ptr1, void* ptr2, int32_t num0)
+	{
+		char buf[512]{};
+		sprintf_s(buf, "%s: %p %p %p %i", __FUNCTION__, ptr0, ptr1, ptr2, num0);
+
+		OutputDebugStringA(buf);
+	}
 }
